bd1687afe2c050c7bb84d007d0c8e61ded1d3e52
Kevin <email.savin.mak@gmail.com>
2024-08-02 08:11:24 -0700

    Cache to enable offline usage

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,30 @@
 self.addEventListener('install', function(event) {
-    console.log('Service Worker installing.');
-  });
-  
-  self.addEventListener('activate', function(event) {
-    console.log('Service Worker activating.');
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          return response || fetch(event.request);
-        })
-    );
-  });
+  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open('pwa-cache-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/details.html',
+        '/input.html',
+        '/script.js',
+        '/manifest.json',
+        '/assets/images/settings_icon_192.png',
+        '/assets/images/settings_icon_512.png',
+        '/assets/images/main-page_bottom.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
